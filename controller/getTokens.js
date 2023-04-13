1b9064eddc01ea335d57b7f09946f2856d968c27
shrikrushana <papupingale123@gmail.com>
2023-04-13 15:10:47 +0530

    fix

--- controller/getTokens.js
+++ controller/getTokens.js
@@ -4,8 +4,8 @@ async function getToken(token) {
   const data = {
     grant_type: 'authorization_code',
     code: token,
-    client_id: '263251679518-nnq7mtohvg65tpfahigqh73kfourr8eh.apps.googleusercontent.com',
-    client_secret: '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',
+    client_id: process.env.GOOGLE_CLIENT_ID,
+    client_secret: process.env.GOOGLE_CLIENT_SECRETs,
     redirect_uri: 'http://localhost:3000/redirect'
   };
 
